be6094eabba87c6a8c7f87ed03d259bd0d1a3415
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-25 16:25:58 -0700

    add response (mad lib style), examples of input types to modify later

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@
 $(document).ready(function() {
  
 });
+
+// text input
+
+
+// select box (list)
+var beverage = $("#beverage").val();
+
+// radio buttons
+var flavor = $("input:radio[name=flavor]:checked").val();
+
+// date
+var dob = $("#born").val();
+
+// color
+var favoriteColor = $("#color").val();
