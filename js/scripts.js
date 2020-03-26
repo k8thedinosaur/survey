f6a7f7a4409152664e8d314acce5e99ae0e6f0fa
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-25 20:45:06 -0700

    break jq by trying to emulate a non-relevant example

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,50 @@
 
 // ui logic
 $(document).ready(function() {
- 
+  $("#survey form").submit(function() {
+  var responses = ["name", "cat-name", "date", "type", "purr", "meow", "color"];
+
+  responses.forEach(function(answer) {
+    var userInput = $("input#" + answer).val();
+    $("." + answer).text(userInput).val();
+  })
+
+  $("#result").show();
+
+  event.preventDefault();
 });
 
-// text input
-var name = $("name").val();
-var catName = $("cat-name").val();
+// // text input
+// var name = $("name").val();
+// var catName = $("cat-name").val();
 
-// select box (list)
-var type = $("#type").val();
+// // date
+// var date = $("#date").val();
 
-// radio buttons
-var purr = $("input:radio[name=purr]:checked").val();
-var meow = $("input:radio[name=meow]:checked").val();
+// // select box (list)
+// var type = $("#type").val();
 
-// date
-var date = $("#date").val();
+// // radio buttons
+// var purr = $("input:radio[name=purr]:checked").val();
+// var meow = $("input:radio[name=meow]:checked").val();
 
-// color
-var color = $("#color").val();
+// // color
+// var color = $("#color").val();
+
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    // alert('Got to beginning of form submit!');
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      // console.log(userInput);
+      $("." + blank).text(userInput).val();      
+    });
+
+    $("#story").show();
+    // alert('Got to end of form submit!');
+
+    event.preventDefault();
+  });
+});
